ad90c67f5f0e45f3441bca66856dd0bb2d0777bd
b-aq <bashar.3akleh_92@hotmail.com>
2021-08-04 23:26:03 +0300

    fixed the backend data

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
 'use strict';
-// http://localhost:3030/weather
+// http://localhost:8081/weather?searchQuery=amman
+require('dotenv').config();
 const express = require('express'); // require the express package
 const cors = require('cors');
 const app = express(); // initialize your express app instance
-require('dotenv').config();
 const PORT = process.env.PORT;
 const weatherData = require('./data/weather.json');
+const response = express();
 
-app.use(cors());
-app.use(express.json());
+response.use(cors());
+response.use(express.json());
 
 app.get('/weather', (req, res) => {
   const lon = req.query.lon;
   const lat = req.query.lat;
   const searchQuery = req.query.searchQuery;
 
-  console.log(lat);
-  console.log(lon);
+  // console.log(lat);
+  // console.log(searchQuery);
 
-  try {
-    let newArray = weatherData.find((item) => {
-      return item.city_name.toLowerCase() === searchQuery.toLowerCase();
-    });
+  // try {
+  let cityNewArray = weatherData.find((element) => {
+    if (element.city_name === searchQuery) {
+      return element.city_name;
+    }else{}
 
-    class Forecast {
-      constructor(datetime, description) {
-        this.date = datetime;
-        this.description = description;
-      }
+  });
+  class Forecast {
+    constructor(datetime, description) {
+      this.date = datetime;
+      this.description = description;
     }
-
-    let array = [];
-    weatherData.data.map((item) => {
-      array.push(
-        new Forecast(
-          item.datetime,
-          `Low of ${item.low_temp}, High of ${item.max_temp},${item.weather.description}`
-        )
-      );
-    });
-
-    res.send(newArray);
-  } catch (e) {
-    res.status(404).send('Page is not found 404');
   }
+  console.log(cityNewArray);
+  // res.status(200).send(cityNewArray);
+  let weatherCity =cityNewArray.data.map(element=>{
+    return new Forecast({datetime:element.datetime, description:`Low of ${element.low_temp}, high of ${element.high_temp} with ${element.weather.description}`});
+
+  });
+
+  res.send(weatherCity);
+
+  // } catch (e) {
+  // res.status(404).send('Page is not found 404');
+  // }
 });
 
 app.listen(PORT, () => {
