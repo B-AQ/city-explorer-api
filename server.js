8f79bb256e725bbd112fd7fdacf7aec4be1b71e9
b-aq <bashar.3akleh_92@hotmail.com>
2021-08-06 22:21:27 +0300

    finished the weatherbit

--- server.js
+++ server.js
@@ -3,10 +3,14 @@
 require('dotenv').config();
 const express = require('express'); // require the express package
 const cors = require('cors');
+const axios = require('axios');
 const app = express(); // initialize your express app instance
-const PORT = process.env.PORT;
 const weatherData = require('./data/weather.json');
+const Forecast = require('./models/Forecast');
+const PORT = process.env.PORT;
 
+const WeatherBit_Key = process.env.WEATHERBIT_KEY;
+const WeatherBit_Url = process.env.WEATHERBIT_URL;
 app.use(cors());
 app.use(express.json());
 
@@ -45,7 +49,25 @@ app.get('/weather', (req, res) => {
   }
 });
 
+//--------------------------------------------------------//
 
+
+
+app.get('/weather-bit',async (req,res) =>{
+  const {latitude,longitude} = req.query;
+  const queryParams={
+    params:{
+      key:WeatherBit_Key,
+      lat:latitude,
+      lon:longitude,
+    }
+  };
+
+  const response = await axios.get(WeatherBit_Url,queryParams);
+  const WeatherBitData = response.data.data.map(item => new Forecast(item));
+  res.json(WeatherBitData);
+
+});
 
 app.listen(PORT, () => {
   console.log(PORT);
