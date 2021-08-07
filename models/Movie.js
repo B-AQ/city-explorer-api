19762af3e02e49a0cd76be895757773ac74d04c8
b-aq <bashar.3akleh_92@hotmail.com>
2021-08-07 23:05:35 +0300

    made some refactoring

--- models/Movie.js
+++ models/Movie.js
@@ -1,13 +1,13 @@
 
 class Movie{
   constructor(data){
-    this.title = data.title;
+    this.original_title = data.title;
     this.overview = data.overview;
     this.vote_average = data.vote_average;
     // this.poster_path = data.poster_path;
     this.poster_path = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
     this.popularity = data.popularity;
-    this.released_on = data.release_date;
+    this.release_date = data.release_date;
 
   }
 }
